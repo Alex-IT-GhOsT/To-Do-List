69b2424cd433fb344e0f5ed8738370d75302ce17
_Alex_ <aleksandr.golubev@ukids.ru>
2025-07-29 00:28:44 +0400

    Создание методов в контроллере

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,34 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Requests\UpdateTaskRequest;
 use App\Services\TaskService\TaskService;
-use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
 
 class TaskController extends Controller
 {
-    public function index(Request $request, TaskService $taskService )
+    public function index(TaskService $taskService ): JsonResponse
     {
-
+        $tasks = $taskService->getAll();
+        return response()->json($tasks);
     }
 
-    public function store(Request $request, TaskService $taskService)
+    public function store(StoreTaskRequest $request, TaskService $taskService): JsonResponse
     {
-
+        $task = $taskService->create($request->validated());
+        return response()->json($task);
     }
 
-    public function show($id, TaskService $taskService)
+    public function show(int $id, TaskService $taskService): JsonResponse
     {
-
+        $task = $taskService->getById($id);
+        return response()->json($task);
     }
 
-    public function update(Request $request, $id, TaskService $taskService)
+    public function update(UpdateTaskRequest $request, int $id, TaskService $taskService): JsonResponse
     {
-
+        $task = $taskService->update($id, $request->validated());
+        return response()->json($task);
     }
 
-    public function destroy($id, TaskService $taskService)
+    public function destroy(int $id, TaskService $taskService): JsonResponse
     {
-
+        $taskService->delete($id);
+        return response()->json(['message' => "Task deleted successfully."]);
     }
 
 }
