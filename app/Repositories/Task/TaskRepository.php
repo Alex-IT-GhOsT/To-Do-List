dea9e2b85ff963bd128a73193656cc9dbb6754c0
_Alex_ <aleksandr.golubev@ukids.ru>
2025-07-28 22:20:00 +0400

    Создание TaskRepository и реализация методов от интерфейса

---
+++ app/Repositories/Task/TaskRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Task;
+
+class TaskRepository implements RepositoryInterface
+{
+    public function all(): array
+    {
+        return Task::all()->toArray();
+    }
+
+    public function find(string $id): ?Task
+    {
+        return Task::fing($id);
+    }
+
+    public function create(array $data): Task
+    {
+        return Task::create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        $task = Task::find($id);
+        return $task ? $task->update($id, $data) : false;
+    }
+
+    public function delete(int $id): bool
+    {
+        $task = Task::find($id);
+        return $task ? $task->delete($id) : false;
+    }
+}
