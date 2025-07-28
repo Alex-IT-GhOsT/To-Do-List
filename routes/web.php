28fa4b94ef2b87a6abe1b22297428e80880b06ff
_Alex_ <aleksandr.golubev@ukids.ru>
2025-07-29 00:02:56 +0400

    Создание маршрутов для API

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/tasks', [TaskController::class, 'index']);
+Route::get('/tasks/{taskId}', [TaskController::class, 'show']);
+Route::post('/tasks', [TaskController::class, 'store']);
+Route::put('/tasks/{taskId}', [TaskController::class, 'update']);
+Route::delete('/tasks/{taskId}', [TaskController::class, 'destroy']);
